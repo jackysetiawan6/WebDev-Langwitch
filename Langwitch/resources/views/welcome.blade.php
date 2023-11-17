69a7fc4d623868b7e31383846d17b5f4d30a2cb2
ichsan <ichsanilyasa3@gmail.com>
2023-11-17 14:54:58 +0700

    ignore test

---
+++ Langwitch/resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<html>
+
+    <head>
+        @push('css')
+            <link rel="stylesheet" href="../app.css">
+        @endpush
+        <h1>bapaklu</h1>
+    </head>
+
+    <body>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga quibusdam fugit sapiente, quia eaque obcaecati
+            ullam architecto! At iure aut possimus, ex voluptatum modi praesentium aliquid veritatis quisquam, minus
+            dolore!</p>
+    </body>
+
+</html>
