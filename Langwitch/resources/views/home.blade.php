bee53b8214eed70a4f457c6db71b57821f0eedce
LAPTOP-TTUIQ39L\ichsan <ichsanilyasa3@gmail.com>
2023-11-22 13:23:33 +0700

    navbar update V1.0.1

---
+++ Langwitch/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <h1>tolol</h1>
+    </body>
+</html>
---
+++ Langwitch/resources/views/navbar.blade.php
@@ -0,0 +1,49 @@
+<html>
+
+    <head>
+        <style>
+            * {
+                padding: 0;
+                margin: 0;
+            }
+
+            .nav {
+                background-color: #FFFFFF;
+                justify-content: space-between;
+                display: flex;
+                padding: 0 10%;
+                height: 120px;
+                box-shadow: 5px 10px #888888;
+            }
+
+            .image-logo {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+
+            .button-login {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }
+
+            .login-text {
+                Padding: 11px 34px 11px 34px;
+                border-radius: 100px;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div class="nav">
+            <div class="image-logo">
+                <h3>image</h3>
+            </div>
+            <div class="button-login">
+                <button class="login-text">Login</button>
+            </div>
+        </div>
+    </body>
+
+</html>
