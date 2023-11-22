42c3c654355ffdbcac3481f5bc5deadc82fb3d3b
Jacky Setiawan <jackysetiawan6@gmail.com>
2023-11-22 16:33:01 +0700

    Login v1.0.2

--- Langwitch/public/js/login.js
+++ Langwitch/public/js/login.js
@@ -1,4 +1,30 @@
+const title = document.querySelector('.middle-title');
+const button = document.querySelector('.middle-button');
+const question = document.querySelector('.middle-question > span');
+const changer = document.querySelector('.middle-question > a');
+const fullname = document.querySelectorAll('.middle > div')[0];
+const email = document.querySelectorAll('.middle > div')[1];
+const password = document.querySelectorAll('.middle > div')[2];
+const confirmation = document.querySelectorAll('.middle > div')[3];
+
 function change_title() {
-    var title = document.getElementById("title");
-    title.innerHTML = "Login";
+    if (title.innerHTML === 'Masuk Akun') {
+        title.innerHTML = 'Daftar Akun';
+        button.innerHTML = 'Daftar';
+        question.innerHTML = 'Sudah punya akun? ';
+        changer.innerHTML = 'Masuk';
+        fullname.style.display = 'block';
+        email.style.display = 'block';
+        password.style.display = 'block';
+        confirmation.style.display = 'block';
+    } else {
+        title.innerHTML = 'Masuk Akun';
+        button.innerHTML = 'Masuk';
+        question.innerHTML = 'Belum punya akun? ';
+        changer.innerHTML = 'Daftar';
+        fullname.style.display = 'none';
+        email.style.display = 'block';
+        password.style.display = 'block';
+        confirmation.style.display = 'none';
+    }
 }
--- Langwitch/resources/views/login.blade.php
+++ Langwitch/resources/views/login.blade.php
@@ -13,14 +13,35 @@
     <div class="container">
         <div class="middle">
             <p class="middle-title">Masuk Akun</p>
-            <div class="name"></div>
-            <div class="email"></div>
-            <div class="password"></div>
-            <div class="confirmation"></div>
+            <div style="display: none;">
+                <svg viewBox="0 0 16 16">
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664z" />
+                </svg>
+                <input type="text" placeholder="Nama Lengkap">
+            </div>
+            <div>
+                <svg viewBox="0 0 16 16">
+                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
+                </svg>
+                <input type="email" placeholder="Email">
+            </div>
+            <div>
+                <svg viewBox="0 0 16 16">
+                    <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8m4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5" />
+                    <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+                </svg>
+                <input type="password" placeholder="Kata Sandi">
+            </div>
+            <div style="display: none;">
+                <svg viewBox="0 0 16 16">
+                    <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2M5 8h6a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V9a1 1 0 0 1 1-1" />
+                </svg>
+                <input type="password" placeholder="Konfirmasi Kata Sandi">
+            </div>
             <button class="middle-button">Masuk</button>
             <div class="middle-question">
                 <span>Tidak memiliki akun?</span>
-                <a onclick="change_title()">Daftar</a>
+                <a onclick="change_title()"> Daftar</a>
             </div>
         </div>
     </div>
