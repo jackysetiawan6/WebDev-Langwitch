7892d912b339c1647a5ad37431b23ccc6f8bc216
Jacky Setiawan <jackysetiawan6@gmail.com>
2023-11-22 14:18:57 +0700

    Migrating resources

--- Langwitch/resources/views/login.blade.php
+++ Langwitch/resources/views/login.blade.php
@@ -1,1 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Langwitch - Learn languages by yourself</title>
+    <link rel="stylesheet" href="{{ asset('css/login.blade.css') }}">
+    <link rel="shortcut icon" href="{{ asset('images/Logo.svg') }}" type="image/x-icon">
+</head>
+
+<body>
+    <div class="container">
+
+    </div>
+</body>
+
+</html>
