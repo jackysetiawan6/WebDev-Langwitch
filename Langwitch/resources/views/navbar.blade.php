f4fcbf05e26f99ca6ba86a41e55a4b534bb99e39
LAPTOP-TTUIQ39L\ichsan <ichsanilyasa3@gmail.com>
2023-11-22 15:52:18 +0700

    Home Update V1.1.3

--- Langwitch/resources/views/navbar.blade.php
+++ Langwitch/resources/views/navbar.blade.php
@@ -1,60 +1,13 @@
 <html>
 
     <head>
-        <style>
-            @import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;1,200;1,300;1,400&display=swap');
-
-            * {
-                padding: 0;
-                margin: 0;
-            }
-
-            .nav {
-                background-color: #FFFFFF;
-                justify-content: space-between;
-                display: flex;
-                padding: 0 10%;
-                height: 120px;
-                box-shadow: 0 1px 1px rgba(0, 0, 0, 0.11),
-                    0 2px 2px rgba(0, 0, 0, 0.11),
-                    0 4px 4px rgba(0, 0, 0, 0.11),
-                    0 8px 8px rgba(0, 0, 0, 0.11),
-                    0 16px 16px rgba(0, 0, 0, 0.11),
-                    0 32px 32px rgba(0, 0, 0, 0.11);
-            }
-
-            .image-logo {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-            }
-
-
-            .button-login {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-            }
-
-            .login-text {
-                Padding: 11px 34px 11px 34px;
-                border-radius: 100px;
-                background: #342265;
-                font-family: 'Nunito', sans-serif;
-                color: #FFFFFF;
-                text-decoration: none;
-            }
-
-            .image-logo {
-                width: 100px;
-            }
-        </style>
+        <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
     </head>
 
     <body>
         <div class="nav">
             <div class="image-logo">
-                <img src="{{ asset('/Image/Logo.svg') }}" alt="" class="image-logo">
+                <img src="{{ asset('/Images/Logo.svg') }}" alt="" class="image-logo">
             </div>
             <div class="button-login">
                 <a href="login" class="login-text">Masuk</a>
