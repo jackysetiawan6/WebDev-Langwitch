489feaf59c724b0f089f4bbf15d83fa36368af77
Jacky Setiawan <jackysetiawan6@gmail.com>
2023-11-24 21:40:40 +0700

    Login v1.0.3

--- Langwitch/public/js/login.js
+++ Langwitch/public/js/login.js
@@ -1,30 +1,40 @@
-const title = document.querySelector('.middle-title');
-const button = document.querySelector('.middle-button');
-const question = document.querySelector('.middle-question > span');
-const changer = document.querySelector('.middle-question > a');
-const fullname = document.querySelectorAll('.middle > div')[0];
-const email = document.querySelectorAll('.middle > div')[1];
-const password = document.querySelectorAll('.middle > div')[2];
-const confirmation = document.querySelectorAll('.middle > div')[3];
+const title = document.querySelector(".middle-title");
+const button = document.querySelector(".middle-button");
+const question = document.querySelector(".middle-question > span");
+const fullname = document.querySelectorAll(".middle > div")[0];
+const email = document.querySelectorAll(".middle > div")[1];
+const password = document.querySelectorAll(".middle > div")[2];
+const confirmation = document.querySelectorAll(".middle > div")[3];
 
 function change_title() {
-    if (title.innerHTML === 'Masuk Akun') {
-        title.innerHTML = 'Daftar Akun';
-        button.innerHTML = 'Daftar';
-        question.innerHTML = 'Sudah punya akun? ';
-        changer.innerHTML = 'Masuk';
-        fullname.style.display = 'block';
-        email.style.display = 'block';
-        password.style.display = 'block';
-        confirmation.style.display = 'block';
+    if (title.innerHTML === "Masuk Akun") {
+        title.innerHTML = "Daftar Akun";
+        button.innerHTML = "Daftar";
+        question.innerHTML =
+            'Sudah punya akun? <a onclick="change_title()">Masuk</a>';
+        fullname.style.display = "flex";
+        email.style.display = "flex";
+        password.style.display = "flex";
+        confirmation.style.display = "flex";
     } else {
-        title.innerHTML = 'Masuk Akun';
-        button.innerHTML = 'Masuk';
-        question.innerHTML = 'Belum punya akun? ';
-        changer.innerHTML = 'Daftar';
-        fullname.style.display = 'none';
-        email.style.display = 'block';
-        password.style.display = 'block';
-        confirmation.style.display = 'none';
+        title.innerHTML = "Masuk Akun";
+        button.innerHTML = "Masuk";
+        question.innerHTML =
+            'Belum punya akun? <a onclick="change_title()">Daftar</a>';
+        fullname.style.display = "none";
+        email.style.display = "flex";
+        password.style.display = "flex";
+        confirmation.style.display = "none";
+    }
+}
+
+function reveal_password(input_id) {
+    const textbox = document.getElementById(input_id);
+    const eye = document.querySelector;
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
     }
 }
