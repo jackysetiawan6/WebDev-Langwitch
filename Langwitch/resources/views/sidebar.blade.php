d6991ba7341230087656f9b11d3834257c619096
LAPTOP-TTUIQ39L\ichsan <ichsanilyasa3@gmail.com>
2023-11-25 21:56:49 +0700

    Update Sidebar + dark mode BETA V1.3.3 (Not Fixed)

--- Langwitch/resources/views/sidebar.blade.php
+++ Langwitch/resources/views/sidebar.blade.php
@@ -5,51 +5,84 @@
     </head>
 
     <body>
-        <div class="night-mode">
-            <a href=""><img src="{{ asset('/Images/Moon.svg') }}" alt="" class="logo-list">
-            </a>
+        <div class="night-mode-toggle">
         </div>
-        <div class="sidebar">
-            <div class="sidebar-top">
-                <img src="{{ asset('/Images/Logo.svg') }}" alt="" class="image-logo-sidebar">
+        <div class="container">
+            <div class="sidebar">
+                <div class="sidebar-top">
+                    <img src="{{ asset('/Images/Logo.svg') }}" alt="" class="image-logo-sidebar">
+                </div>
+                <div class="list-sidebar">
+                    <ul>
+                        <li>
+                            <a href=""><img src="{{ asset('/Images/BookShiny.svg') }}" alt="" class="logo-list">
+                                <p class="change-dark-mode">Belajar</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href=""><img src="{{ asset('/Images/LeaderBoardLogo.svg') }}" alt="" class="logo-list">
+                                <p class="change-dark-mode-1">Papan Skor</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href=""><img src="{{ asset('/Images/Messages.svg') }}" alt="" class="logo-list">
+                                <p class="change-dark-mode-2">Diskusi</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href=""><img src="{{ asset('/Images/HumanProfile.svg') }}" alt="" class="logo-list">
+                                <p class="change-dark-mode-3">Profil</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href=""><img src="{{ asset('/Images/Translate.svg') }}" alt="" class="logo-list">
+                                <p class="change-dark-mode-4">Terjemahan</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="exit-sidebar">
+                    <li>
+                        <a href=""><img src="{{ asset('/Images/ExitButton.svg') }}" alt="" class="logo-list">
+                            <p class="change-dark-mode-5">Keluar</p>
+                        </a>
+                    </li>
+                </div>
             </div>
-            <div class="list-sidebar">
-                <ul>
-                    <li>
-                        <a href=""><img src="{{ asset('/Images/BookShiny.svg') }}" alt="" class="logo-list">
-                            <p>Belajar</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href=""><img src="{{ asset('/Images/LeaderBoardLogo.svg') }}" alt="" class="logo-list">
-                            <p>Papan Skor</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href=""><img src="{{ asset('/Images/Messages.svg') }}" alt="" class="logo-list">
-                            <p>Diskusi</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href=""><img src="{{ asset('/Images/HumanProfile.svg') }}" alt="" class="logo-list">
-                            <p>Profil</p>
-                        </a>
-                    </li>
-                    <li>
-                        <a href=""><img src="{{ asset('/Images/Translate.svg') }}" alt="" class="logo-list">
-                            <p>Terjemahan</p>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="exit-sidebar">
-                <li>
-                    <a href=""><img src="{{ asset('/Images/ExitButton.svg') }}" alt="" class="logo-list">
-                        <p>Keluar</p>
-                    </a>
-                </li>
+            <div class="container-content-bg">
             </div>
         </div>
+
+        <script>
+            const modeToggleElements = document.querySelectorAll('.night-mode-toggle');
+            const darkModeElement = document.querySelector('.container-content-bg');
+            const darkmodesidebar = document.querySelector('.sidebar');
+            const darkmodetext = document.querySelector('.change-dark-mode');
+            const darkmodetext2 = document.querySelector('.change-dark-mode-1');
+            const darkmodetext3 = document.querySelector('.change-dark-mode-2');
+            const darkmodetext4 = document.querySelector('.change-dark-mode-3');
+            const darkmodetext5 = document.querySelector('.change-dark-mode-4');
+            const darkmodetext6 = document.querySelector('.change-dark-mode-5');
+            const borderchange = document.querySelector('.list-sidebar ul li');
+
+            const handleModeToggle = () => {
+                darkModeElement.classList.toggle('active');
+                darkmodesidebar.classList.toggle('active');
+                darkmodetext.classList.toggle('active');
+                darkmodetext2.classList.toggle('active');
+                darkmodetext3.classList.toggle('active');
+                darkmodetext4.classList.toggle('active');
+                darkmodetext5.classList.toggle('active');
+                darkmodetext6.classList.toggle('active');
+                borderchange.classList.toggle('active');
+
+
+
+                modeToggleElements.forEach((element) => element.classList.toggle('active'));
+            };
+
+            modeToggleElements.forEach((element) => element.addEventListener('click', handleModeToggle));
+        </script>
     </body>
 
 </html>
