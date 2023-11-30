7e45e78266269cb0617ce7e510377b66e1fd70fc
Jacky Setiawan <jackysetiawan6@gmail.com>
2023-11-30 15:34:07 +0700

    Login v1.0.5

--- Langwitch/database/migrations/database_create_users_table.php
+++ Langwitch/database/migrations/database_create_users_table.php
@@ -6,25 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->char('id', length: 5)->primary;
+            $table->string('nama_lengkap');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->dateTime('registered_date');
+            $table->dateTime('last_online_date');
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
--- Langwitch/resources/views/login.blade.php
+++ Langwitch/resources/views/login.blade.php
@@ -14,28 +14,33 @@
         <img src="{{ asset('images/Logo.svg') }}" alt="Logo" class="logo">
         <form class="middle">
             <p class="middle-title">Masuk Akun</p>
-            <div style="display: none;">
+            <div id="fullname" style="display: none;">
                 <img class="input-icon" src="{{ asset('images/Fullname.svg') }}"></img>
-                <input id="fullname-input" type="text" placeholder="Nama Lengkap" required>
+                <input id="fullname-input" type="text" placeholder="Nama Lengkap" autocomplete="off">
             </div>
-            <div>
+            <p id="invalid-fullname" class="invalid-input"></p>
+            <div id="email">
                 <img class="input-icon" src="{{ asset('images/Email.svg') }}"></img>
-                <input id="email-input" type="email" placeholder="Email" required>
+                <input id="email-input" type="email" placeholder="Email" autocomplete="off">
             </div>
-            <div>
+            <p id="invalid-email" class="invalid-input"></p>
+            <div id="password">
                 <img class="input-icon" src="{{ asset('images/Password.svg') }}"></img>
-                <input id="password-input" type="password" placeholder="Kata Sandi" required>
-                <img id="password-eye" class="hide-password" src="{{ asset('images/EyeClosed.svg') }}" onclick="reveal_password('password')"></img>
+                <input id="password-input" type="password" placeholder="Kata Sandi" autocomplete="off">
+                <img id="password-eye" class="hide-password" src="{{ asset('images/EyeClosed.svg') }}" onmousedown="reveal_password('password')" onmouseup="hide_password('password')"></img>
             </div>
-            <div style="display: none;">
+            <p id="invalid-password" class="invalid-input"></p>
+            <div id="confirmation" style="display: none;">
                 <img class="input-icon" src="{{ asset('images/Confirmation.svg') }}"></img>
-                <input id="confirmation-input" type="password" placeholder="Konfirmasi Kata Sandi" required>
-                <img id="confirmation-eye" class="hide-password" src="{{ asset('images/EyeClosed.svg') }}" onclick="reveal_password('confirmation')"></img>
+                <input id="confirmation-input" type="password" placeholder="Konfirmasi Kata Sandi" autocomplete="off">
+                <img id="confirmation-eye" class="hide-password" src="{{ asset('images/EyeClosed.svg') }}" onmousedown="reveal_password('confirmation')" onmouseup="hide_password('confirmation')"></img>
             </div>
+            <p id="invalid-confirmation" class="invalid-input"></p>
             <progress max="100" value="0"></progress>
-            <button class="middle-button" onclick="submit_data()">Masuk</button>
+            <button class="middle-button" type="button" onclick="validate()">Masuk</button>
             <div class="middle-question">
-                <span>Belum punya akun? <a onclick="change_title()">Daftar</a></span>
+                <span>Belum punya akun?</span>
+                <a onclick="toggle_login_register()">Daftar</a>
             </div>
         </form>
     </div>
