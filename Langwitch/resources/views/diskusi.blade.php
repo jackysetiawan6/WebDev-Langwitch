fa21467e793619f6b75b5fae8254b2c88a8ffebb
LAPTOP-TTUIQ39L\ichsan <ichsanilyasa3@gmail.com>
2024-02-01 15:40:40 +0700

    discussion 90% done

--- Langwitch/resources/views/diskusi.blade.php
+++ Langwitch/resources/views/diskusi.blade.php
@@ -15,19 +15,37 @@
         <div class="container">
             @include('sidebar')
             <div class="container-content-bg">
-                <div class="diskusi-container">
-                    <p class="new-post">New Post</p>
-                    <form class="text-button" method="post" action="{{ route('diskusi-store') }}" enctype="multipart/form-data">
-                        @csrf
-                        <div class="text">
-                            <input type="text" placeholder="Type a text" id="finasim" name="text">
-                            <input type="file" name="" id="file-input" accept="image/*" onchange="previewImage();" name="image">
-                            <img id="preview">
-                            <label for="file-input" id="file-input-label"><img src="{{ asset('images/attachment.png') }}" alt=""></label>
+                <div class="container-all">
+                    <div class="diskusi-container">
+                        <p class="new-post">New Post</p>
+                        <form class="text-button" method="post" action="{{ route('diskusi-store') }}" enctype="multipart/form-data">
+                            @csrf
+                            <div class="text">
+                                <input type="text" placeholder="Type a text" id="finasim" name="text">
+                                <input type="file" name="" id="file-input" accept="image/*" onchange="previewImage();" name="image">
+                                <img id="preview">
+                                <label for="file-input" id="file-input-label"><img src="{{ asset('images/attachment.png') }}" alt=""></label>
+                            </div>
+                            <button type="submit" class="button-post">Post</button>
+                        </form>
+                    </div>
+                    <div class="discussion-other-contain">
+                        <div class="prof-name">
+                            <div class="prof-pic"><img src="{{ asset('/Images/profpic.svg') }}" alt=""></div>
+                            <div class="name-stats-time">
+                                {{-- @foreach ($User as $user) --}}
+                                <div class="name-disc">Nana</div>
+                                <div class="time-disc">10 mins ago</div>
+                                {{-- @endforeach --}}
+                            </div>
+                            <div class="rank-disc">[Wizard]</div>
                         </div>
-                        <button type="submit" class="button-post">Post</button>
-                    </form>
-
+                        <div class="text-2">
+                            @foreach ($Discussion as $discussion)
+                                {{ $discussion->text }}
+                            @endforeach
+                        </div>
+                    </div>
                 </div>
             </div>
             <script type="text/javascript" src="{{ asset('js/course.js') }}"></script>
