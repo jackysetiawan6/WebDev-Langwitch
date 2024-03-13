eee0e3b6d85412f6af70d0563f6e4b306378c6e4
LAPTOP-TTUIQ39L\ichsan <ichsanilyasa3@gmail.com>
2024-03-13 16:34:09 +0700

    update done 2024

--- Langwitch/resources/views/testcoursefirst.blade.php
+++ Langwitch/resources/views/testcoursefirst.blade.php
@@ -19,12 +19,14 @@
             <div class="title-container">
                 Course 1: Vocabulary
             </div>
-            {{ $Question->links() }}
+            {{-- {{ $Question->links() }}
             @php
                 use Spatie\LaravelHtml\HtmlFacade as Html;
+                $currentPage = $Question->currentPage();
+                $lastPage = $Question->lastPage();
             @endphp
 
-            <form action="{{ route('submit_answers', ['page' => $currentPage ?? 1]) }}" method="post">
+            <form action="{{ route('submit_answers', ['page' => $currentPage]) }}" method="post">
                 @csrf
 
                 @foreach ($Question as $q)
@@ -43,9 +45,58 @@
                 @endforeach
 
                 <div class="button-answer">
-                    <button class="answer-the-question" type="submit">Jawab</button>
+                    @if ($currentPage == $lastPage)
+                        <a href="{{ url('/course') }}" class="answer-the-question">Done</a>
+                    @else
+                        <button class="answer-the-question" type="submit">Jawab</button>
+                    @endif
+                </div>
+            </form> --}}
+
+            {{ $Question->links() }}
+            @php
+                use Spatie\LaravelHtml\HtmlFacade as Html;
+                $currentPage = $Question->currentPage();
+                $lastPage = $Question->lastPage();
+            @endphp
+
+            <form id="questionForm" action="{{ route('submit_answers', ['page' => $currentPage]) }}" method="post">
+                @csrf
+
+                @foreach ($Question as $q)
+                    <div class="soal-course-first">
+                        {{ $q['soal'] }}
+                    </div>
+                    <div class="jawaban-course-first">
+                        @foreach (['opt_a', 'opt_b', 'opt_c', 'opt_d', 'opt_e'] as $optionKey)
+                            <label class="radio-label">
+                                <input type="radio" name="answers[{{ $q['exercise_id'] }}]" value="{{ $optionKey }}"
+                                    class="ans-effect activator">
+                                <span class="radio-text">{{ $q[$optionKey] }}</span>
+                            </label>
+                        @endforeach
+                    </div>
+                @endforeach
+
+                <div class="button-answer">
+                    @if ($currentPage == $lastPage)
+                        <button class="answer-the-question" type="button" onclick="doneButtonClicked()">Done</button>
+                    @else
+                        <button class="answer-the-question" type="submit">Jawab</button>
+                    @endif
                 </div>
             </form>
+
+            <script>
+                function doneButtonClicked() {
+                    var radios = document.querySelectorAll('input[type=radio]:checked');
+                    if (radios.length < {{ count($Question) }}) {
+                        alert("Please answer all the questions before proceeding.");
+                    } else {
+                        document.getElementById('questionForm').submit();
+                    }
+                }
+            </script>
 
             @if (isset($results))
                 <div class="results">
